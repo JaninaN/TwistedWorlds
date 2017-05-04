1d6c988a0db167032fe971bf8b892cde12e5eec2
Jini <Janina.Noell@gmx.de>
2017-05-04 16:16:40 +0200

    Erster Aufbau mit Testhintergrund und zwei Charakteren. Eventsystem eingerichtet.

---
+++ Assets/Scripts/OnClickTestEvent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnClickTestEvent : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void onSignClicked()
+    {
+        GameObject testText = new GameObject("SonText");
+        testText.transform.SetParent(this.transform);
+        Text txt = testText.AddComponent<Text>();
+
+        txt.text = "Da sollen wir hin gehen Mama.";
+
+    }
+}
